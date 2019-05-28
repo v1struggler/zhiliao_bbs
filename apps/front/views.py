785a10335c2d465e0bdf7632c345bf862ca6cc41
v1struggler <v1struggler@163.com>
2019-05-28 16:31:41 +0800

    注册页面完成

--- apps/front/views.py
+++ apps/front/views.py
@@ -1,6 +1,15 @@
 # 定义视图
 
-from flask import Blueprint
+from flask import (
+    Blueprint,
+    views,
+    render_template,
+    request,
+    session,
+    url_for,
+    abort,
+    g
+)
 
 bp = Blueprint("front", __name__)
 
@@ -8,3 +17,14 @@ bp = Blueprint("front", __name__)
 @bp.route('/')
 def index():
     return "front index"
+
+
+class SignupView(views.MethodView):
+    def get(self):
+        return render_template('front/front_signup.html')
+
+    def post(self):
+        pass
+
+
+bp.add_url_rule('/signup/', view_func=SignupView.as_view('signup'))
