c0b27f0884206ecad8e58381fc064bea2b7e6ef8
v1struggler <v1struggler@163.com>
2019-05-28 17:43:53 +0800

    生成图形验证码

--- apps/common/views.py
+++ apps/common/views.py
@@ -1,6 +1,9 @@
 # 定义视图
 
-from flask import Blueprint
+from flask import Blueprint, make_response
+from utils.captcha import Captcha
+from io import BytesIO
+from utils import zlcache
 
 bp = Blueprint("common", __name__, url_prefix='/common')
 
@@ -8,3 +11,15 @@ bp = Blueprint("common", __name__, url_prefix='/common')
 @bp.route('/')
 def index():
     return "common index"
+
+
+@bp.route('/captcha/')
+def graph_captcha():
+    text, image = Captcha.gene_graph_captcha()      # 获取验证码
+    zlcache.set(text.lower(), text.lower())
+    out = BytesIO()                                 # 创建字节流
+    image.save(out, 'png')                          # 将图片存储到字节流当中，格式为png
+    out.seek(0)                                     # 将文件流的指针放在0的位置
+    resp = make_response(out.read())                # 创建response对象
+    resp.content_type = 'image/png'
+    return resp
